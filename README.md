06035541f967cf1b1e98f9982333ebb7c958398d
ObradovicNikola <obradovicnikola009@gmail.com>
2019-08-12 10:44:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Go-SMTP
+This is a SMTP (simple mail transfer protocol) client written in Go.
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+func main() {
+
+	sender := NewSender("<YOUR EMAIL ADDRESS>", "<YOUR EMAIL PASSWORD>")
+
+	//The receiver needs to be in slice as the receive supports multiple receiver
+	Receiver := []string{"abc@gmail.com", "xyz@gmail.com", "larrypage@googlemail.com"}
+
+	Subject := "Testing HTLML Email from golang"
+	message := `
+	<!DOCTYPE HTML PULBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+	<html>
+	<head>
+	<meta http-equiv="content-type" content="text/html"; charset=ISO-8859-1">
+	</head>
+	<body>This is the body<br>
+	<div class="moz-signature"><i><br>
+	<br>
+	Regards<br>
+	Sender<br>
+	<i></div>
+	</body>
+	</html>
+	`
+	bodyMessage := sender.WriteHTMLEmail(Receiver, Subject, message)
+
+	sender.SendMail(Receiver, Subject, bodyMessage)
+}
